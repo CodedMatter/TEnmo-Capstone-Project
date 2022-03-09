54bd1e7f1b2405f13c48aa548a9197bdd8e1584d
Ryan Hunter <hunterryan0000@yahoo.com>
2022-03-09 11:39:53 -0600

    Created model classes

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/Transfer.java
@@ -0,0 +1,67 @@
+package com.techelevator.tenmo.model;
+
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+public class Transfer {
+
+    private int id;
+    @NotNull
+    private int transferTypeId;
+    @NotNull
+    private int transferStatusId;
+    @NotNull
+    private Account accountFrom;
+    @NotNull
+    private Account accountTo;
+    @NotNull
+    private BigDecimal amount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTransferTypeId() {
+        return transferTypeId;
+    }
+
+    public void setTransferTypeId(int transferTypeId) {
+        this.transferTypeId = transferTypeId;
+    }
+
+    public int getTransferStatusId() {
+        return transferStatusId;
+    }
+
+    public void setTransferStatusId(int transferStatusId) {
+        this.transferStatusId = transferStatusId;
+    }
+
+    public Account getAccountFrom() {
+        return accountFrom;
+    }
+
+    public void setAccountFrom(Account accountFrom) {
+        this.accountFrom = accountFrom;
+    }
+
+    public Account getAccountTo() {
+        return accountTo;
+    }
+
+    public void setAccountTo(Account accountTo) {
+        this.accountTo = accountTo;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
